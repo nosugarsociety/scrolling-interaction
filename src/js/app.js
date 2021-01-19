5511c629a0482f9322aba26164cc34c33be121a9
Jusang You <everflavor@gmail.com>
2021-01-18 23:24:37 -0500

    Set active class for current area

--- src/js/app.js
+++ src/js/app.js
@@ -43,12 +43,30 @@
   ];
 
   const setLayout = () => {
+    // setting height of each scroll section
     for (let i = 0; i < sceneInfo.length; i++) {
       sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
       sceneInfo[
         i
       ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
+
+    let totaScrolllHeight = 0;
+    // console.log('win pyoff:', window.pageYOffset);
+    for (let i = 0; i < sceneInfo.length; i++) {
+      totaScrolllHeight += sceneInfo[i].scrollHeight;
+
+      if (totaScrolllHeight >= window.pageYOffset) {
+        currentScene = i;
+        console.log('set Layout currentScene:', currentScene);
+        break;
+      }
+    }
+
+    document.body.setAttribute(
+      'class',
+      `scroll-section-${currentScene}--active`
+    );
   };
 
   const scrollLoop = () => {
@@ -60,14 +78,25 @@
 
     if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
       currentScene += 1;
+
+      document.body.setAttribute(
+        'class',
+        `scroll-section-${currentScene}--active`
+      );
     }
 
     if (yOffset < prevScrollHeight) {
       if (currentScene === 0) return;
       currentScene -= 1;
+
+      document.body.setAttribute(
+        'class',
+        `scroll-section-${currentScene}--active`
+      );
     }
   };
 
+  window.addEventListener('load', setLayout);
   window.addEventListener('resize', setLayout);
   window.addEventListener('scroll', () => {
     yOffset = window.pageYOffset;
